88bc50ebe046182e7a42ac63e848baec6a9d7f49
Dan Hariton <dhariton@griddynamics.com>
2025-08-14 16:53:09 +0300

    add header to root layout (#5)

---
+++ hekto/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+import TopBar from "./components/TopBar";
+
+export function Header() {
+  return (
+    <header>
+      <TopBar />
+      <NavBar />
+    </header>
+  );
+}
