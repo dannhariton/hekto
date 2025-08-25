91f306ea9def3b6df072fdee91edd53b469e3b55
Dan Hariton <dhariton@griddynamics.com>
2025-08-25 15:55:46 +0300

    add products listing page component (#17)
    * add filter accordion component
    
    * add products page component
    
    * Revert "add filter accordion component"
    
    This reverts commit 2a9fae1034759dda3a9e3a945d96049f76cfad44.
    
    * remove unused imports and elements from plp

---
+++ hekto/src/app/products/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
+import { colors } from "@/styles/colors";
+import { typography } from "@/styles/typography";
+import { rem } from "@/utils/remConvert";
+import styled from "styled-components";
+
+const S = {
+  Container: styled.div``,
+  Header: styled.h1`
+    ${typography.H1}
+    color: ${colors.BLACK};
+    text-align: center;
+    padding: ${rem(60)} 0 ${rem(80)};
+  `,
+  Content: styled.div`
+    display: flex;
+    margin: ${rem(32)} 0;
+    gap: ${rem(32)};
+    align-items: flex-start;
+  `,
+};
+
+function ProductsPage() {
+  return (
+    <MaxWidthWrapper>
+      <S.Container>
+        <S.Header>Products</S.Header>
+        <S.Content></S.Content>
+      </S.Container>
+    </MaxWidthWrapper>
+  );
+}
+
+export default ProductsPage;
