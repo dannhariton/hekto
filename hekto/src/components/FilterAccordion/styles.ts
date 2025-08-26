b673a940911a01a78e72b7cf4e70559ae4439670
Dan Hariton <dhariton@griddynamics.com>
2025-08-26 15:39:33 +0300

    remove unused styled components

--- hekto/src/components/FilterAccordion/styles.ts
+++ hekto/src/components/FilterAccordion/styles.ts
@@ -7,20 +7,17 @@ import {
   FormGroup,
   RadioGroup,
 } from "@mui/material";
-import styled, { css, RuleSet } from "styled-components";
+import styled from "styled-components";
 
 import { colors } from "@/styles/colors";
 import { typography } from "@/styles/typography";
 import { rem } from "@/utils/remConvert";
 import { Filter } from "./types";
 
-type Styles = Record<Filter, RuleSet>;
-
 export const S = {
   Container: styled.div<{ $type: Filter }>`
     display: flex;
     flex-direction: column;
-    ${({ $type }) => styles[$type]}
   `,
   Accordion: styled(Accordion)`
     border: 1px solid ${colors.GREY_2};
@@ -40,11 +37,4 @@ export const S = {
   FormGroup: styled(FormGroup)``,
   FormControlLabel: styled(FormControlLabel)``,
   Box: styled(Box)``,
-};
-
-const styles: Styles = {
-  "MULTI-SELECT": css``,
-  "SINGLE-SELECT": css``,
-  RANGE: css``,
-  RATING: css``,
 };
